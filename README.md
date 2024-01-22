96dd177b4b2fb03bb39e7e9972874a16e86134d3
Relyful <13381270+Relyful@users.noreply.github.com>
2024-01-22 20:10:55 +0100

    Add photo copyright and Passwords must match text underneath password field if they dont match

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sign up form
 Testing my knowledge of creating a form with form control learned from The Odin Project. 
+Using simple javascript for password matching otherwise pure html / css.
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const password = document.querySelector("#pwd");
 const cpassword = document.querySelector("#cpwd");
+const pmatch = document.querySelector(".errormsg");
 
 function checkPwd() {
     if(password.value == cpassword.value) {
         password.style.border = "1px solid rgb(97, 196, 230)";
         cpassword.style.border = "1px solid rgb(97, 196, 230)";
+        pmatch.style.display = "none";
     }
     else {
         password.style.border = "solid 1px red";
         cpassword.style.border = "solid 1px red";
-        console.log("xdd");
+        pmatch.style.display = "inline";
     }
 }
